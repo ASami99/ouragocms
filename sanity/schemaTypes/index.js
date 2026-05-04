f89399ffd6758b01c7155f67889fa2dfd9424e38
Abdul Sami <samirafiq.sohail@gmail.com>
2026-05-05 00:37:33 +0300

    chore: remove Sanity dependencies

--- sanity/schemaTypes/index.js
+++
@@ -1,7 +0,0 @@
-import restaurant from './restaurant.js'
-import category from './category.js'
-import menuItem from './menuItem.js'
-
-export const schema = {
-  types: [restaurant, category, menuItem],
-}
